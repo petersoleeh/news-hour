b65d9f2b27967ad6aa952764c9eaef3dc4baf058
peter soleeh <petersoleeh@gmail.com>
2017-10-23 00:49:13 +0300

    add class articles

--- app/models/news.py
+++ app/models/news.py
@@ -2,6 +2,7 @@ class News:
     '''
     to define News objects
     '''
+    #sources
 
     def __init__(self, id, name, description, url, category):
         self.id = id
@@ -9,4 +10,19 @@ class News:
         self.description = description
         self.url = url
         self.category = category
-        
+
+#articles
+
+class Article:
+
+    '''
+    to define article objects
+    '''
+
+    def __init__(self,author,title,description,url,publishedAt):
+
+        self.author = author
+        self.title = title
+        self.description =description
+        self.url = url
+        self.publishedAt = publishedAt
